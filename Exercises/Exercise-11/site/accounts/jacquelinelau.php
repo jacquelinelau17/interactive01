376b51aaf1f5aa2466c7145a8f12278e6e1e80e7
jacquelinelau17 <jacqueline.lau.17@gmail.com>
2017-04-25 17:25:43 -0700

    ok

---
+++ Exercises/Exercise-11/site/accounts/jacquelinelau.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: jacquelinelau
+password: >
+  $2a$10$eASpfcHEcmq9WyJQCgWZc.iS9udts9z1BrkuatUFQhn4NxcYgMqjC
+email: jacqueline.lau.17@gmail.com
+language: en
+role: admin
